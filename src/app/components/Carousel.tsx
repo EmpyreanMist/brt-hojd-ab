f9041badd63df364494e3af122096bface9182d3
Christian <christianfryksten1@gmail.com>
2025-07-07 19:24:38 +0200

    update carousel

--- src/app/components/Carousel.tsx
+++ src/app/components/Carousel.tsx
@@ -1,4 +1,5 @@
 "use client";
+import { useState } from "react";
 
 import { Carousel } from "react-bootstrap";
 
@@ -20,24 +21,42 @@ const images = [
   },
 ];
 
-export default function ImageCarousel() {
-  return (
-    <div className="carousel-wrapper text-center">
-      <Carousel fade interval={4000}>
-        {images.map((image, index) => (
-          <Carousel.Item key={index}>
-            <div style={{ display: "inline-block", position: "relative" }}>
-              <img
-                src={image.src}
-                alt={`Bild ${index + 1}`}
-                style={{
-                  width: "100%",
-                  height: "75vh",
-                  objectFit: "contain",
-                  display: "block",
-                }}
-              />
+const getBackgroundColor = (index: number): string => {
+  switch (index) {
+    case 0:
+      return "#d6cfc6"; // Mörk färg för hero1.jpg
+    case 1:
+      return "#e7edf7"; // Mörk färg för hero2.jpg
+    case 2:
+      return "#1e2422"; // Mörk färg för hero3.jpg
+    default:
+      return "#000000";
+  }
+};
 
+export default function ImageCarousel() {
+  const [index, setIndex] = useState(0);
+
+  const handleSelect = (selectedIndex: number) => {
+    setIndex(selectedIndex);
+  };
+
+  const isMobile = typeof window !== "undefined" && window.innerWidth < 768;
+  const bgColor = getBackgroundColor(index);
+
+  return (
+    <div
+      className="carousel-wrapper text-center"
+      style={{
+        backgroundColor: !isMobile ? bgColor : "transparent",
+        transition: "background-color 0.5s ease",
+      }}
+    >
+      <Carousel fade interval={4000} indicators={false} onSelect={handleSelect}>
+        {images.map((image, i) => (
+          <Carousel.Item key={i}>
+            <div className="carousel-item-wrapper">
+              <img src={image.src} alt={`Bild ${i + 1}`} />
               <div
                 style={{
                   position: "absolute",
@@ -46,10 +65,11 @@ export default function ImageCarousel() {
                   backgroundColor: "rgba(0, 0, 0, 0.5)",
                   color: "white",
                   padding: "1rem",
+                  zIndex: 2,
                 }}
               >
-                <h3>{image.title}</h3>
-                <p>{image.description}</p>
+                <h3 className="mainTitle">{image.title}</h3>
+                <p className="subTitle">{image.description}</p>
               </div>
             </div>
           </Carousel.Item>
